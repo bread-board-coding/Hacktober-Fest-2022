aa0e16a12f460b20c5a8d2924e54cf707c695422
iqrafirdose <114678694+iqrafirdose@users.noreply.github.com>
2022-10-04 19:28:30 +0530

    Create three_dimensional_array.java

---
+++ Java/Array/three_dimensional_array.java
@@ -0,0 +1,9 @@
+/*simple program on three dimensional array*/
+public class Array
+{
+  public static void main(String args[])
+  {
+    int[][][] arr={{{1,2,10},{3,4,5}},{{6,4,2},{8,7,9}}};
+    System.out.println("arr[1][1][2]="+arr[1][1][2]);
+  }
+}  
