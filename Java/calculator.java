cfd723141afc6e923b737d2aadd12519ec4acfec
ashutoshak5386 <50767646+ashutoshak5386@users.noreply.github.com>
2022-10-05 15:54:56 +0530

    Create calculator.java

---
+++ Java/calculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class calculator {
+
+   
+    
+
+    public static void main(String[] args) {
+
+        float number_1, number_2;
+        System.out.println("Enter first number");
+
+        Scanner scan = new Scanner(System.in);
+        number_1 = scan.nextFloat();
+
+        System.out.println("Enter second number");
+//        Scanner scan2 = new Scanner(System.in);
+        number_2 = scan.nextFloat();
+        System.out.print("You have Entered ");
+        System.out.print(number_1);
+        System.out.print(" and ");
+        System.out.println(number_2);
+        String prompt = "Enter 0 for addition, 1 for " +
+                        "subtraction, 2 for multiplication and 3 for division";
+        System.out.println(prompt);
+        int input = scan.nextInt();
+        switch (input){
+            case 0:
+                System.out.println("Adding these numbers");
+                System.out.print("The result is: ");
+                System.out.println(number_1 + number_2);
+                break;
+
+            case 1:
+                System.out.println("Subtracting these numbers");
+                System.out.print("The result is: ");
+                System.out.println(number_1 - number_2);
+                break;
+
+            case 2:
+                System.out.println("Multiplying these numbers");
+                System.out.print("The result is: ");
+                System.out.println(number_1 * number_2);
+                break;
+
+            case 3:
+                System.out.println("Dividing these numbers");
+                System.out.print("The result is: ");
+                System.out.println(number_1 / number_2);
+                break;
+
+            default:
+                System.out.println("Invalid input");
+
+        }
+
+
+    }
+}
