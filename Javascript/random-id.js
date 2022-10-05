76ac169e99b7eafd7267152b1e4c35a49a1afb37
akbarsaputrait <akbarsaputrait@outlook.com>
2022-10-05 13:00:05 +0700

    Javascript - Random ID (akbarsaputrait)

---
+++ Javascript/random-id.js
@@ -0,0 +1,18 @@
+/**
+ * Generates a random id
+ *
+ * @param length
+ */
+function randomId(length = 16) {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+
+  for (let i = 0; i < length; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return id;
+}
+
+console.log(randomId());
