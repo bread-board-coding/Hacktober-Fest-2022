d58f4c29746c843ae4a5c34c1972a2745acd6f90
Arsh <arshlakhina2002@gmail.com>
2022-10-09 23:58:15 +0530

    Sum of nodes in a Tree

---
+++ C++/SumofNodesinTree.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int sumOfNodes(TreeNode<int>* root) {
+    // Write your code here
+    int sum=root->data;
+    for(int i=0;i<root->children.size();i++)
+    {
+        sum+=sumOfNodes(root->children[i]);
+    }
+    return sum;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << sumOfNodes(root);
+}
