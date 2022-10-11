fe4f16f8c9b06bf9cfc59f2b2dd0fd289f09e591
yogeeta b <90635845+Yogeeta16@users.noreply.github.com>
2022-10-11 23:15:59 +0530

    playing cats

---
+++ Java/PlayingCat.java
@@ -0,0 +1,12 @@
+public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(false,36));
+        System.out.println(isCatPlaying(false,35));
+
+    }
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int upperLimit = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= upperLimit;
+    }
+}
